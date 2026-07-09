7a042f81ab55acc802ad09ac9cfb0638015c659c
apolkova <a_polkova@utb.cz>
2026-07-09 19:10:01 +0200

    register and login page

---
+++ client/src/pages/Login.tsx
@@ -0,0 +1,10 @@
+function Login() {
+  return (
+    <main>
+      <h1>Login</h1>
+      <p>Login form will go here.</p>
+    </main>
+  );
+}
+
+export default Login;
---
+++ client/src/pages/Register.tsx
@@ -0,0 +1,10 @@
+function Register() {
+  return (
+    <main>
+      <h1>Create Account</h1>
+      <p>Register form will go here.</p>
+    </main>
+  );
+}
+
+export default Register;
