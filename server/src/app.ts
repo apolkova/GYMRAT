c26eeb99ebca8549041ee7cf824d401e4f47156c
apolkova <a_polkova@utb.cz>
2026-07-09 20:10:41 +0200

    connecting express serv to prisma

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,16 +1,30 @@
 import express from "express";
 import cors from "cors";
 
+import prisma from "./lib/prisma.js"; //testing
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/health", (_req, res) => {
-  res.json({
-    status: "ok",
-    message: "GymRat API is running",
-  });
+app.get("/api/db-test", async (_req, res) => {
+  try {
+    const userCount = await prisma.user.count();
+
+    res.json({
+      status: "ok",
+      message: "Database connection is working",
+      userCount,
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      status: "error",
+      message: "Database connection failed",
+    });
+  }
 });
 
 export default app;
